28a7fc32a7e1c26700951fbbe6dbf97f3e369722
DrKins <52624337+DrKins@users.noreply.github.com>
2019-12-25 19:53:20 +0100

    update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
-
-    <router-view/>
+    <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view/>
+      </transition>
   </div>
 </template>
 
 <style>
 body {
+  font-family: 'Ubuntu', sans-serif;
+  margin: 0;
+  padding: 0;
   background-attachment: fixed;
-  background-size: cover;
+  background: linear-gradient(to bottom, #F09819, #FF512F); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  height: 100%;
+  background-repeat: no-repeat;
+  overflow: auto;
+  /*overflow-y: hidden;*/
+  overflow-x: hidden;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
 
-    background: #1f3044;  /* fallback for old browsers */
-    background: -webkit-linear-gradient(to right, #1f3044, #646c79);  /* Chrome 10-25, Safari 5.1-6 */
-    background: linear-gradient(to right, #1f3044, #646c79); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+.fade-enter,
+.fade-leave-active {
+  opacity: 0
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import CircularCountDownTimer from "vue-circular-count-down-timer";
+import VueConfetti from 'vue-confetti';
+import VueHtmlToPaper from 'vue-html-to-paper';
+import ToggleButton from 'vue-js-toggle-button';
+ 
+Vue.use(ToggleButton);
+
+const options = {
+  name: '_blank',
+  specs: [
+    'fullscreen=yes'
+  ],
+  styles: [
+    'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css',
+  ]
+}
+
+Vue.use(VueHtmlToPaper, options);
+Vue.use(CircularCountDownTimer);
+Vue.use(VueConfetti);
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
