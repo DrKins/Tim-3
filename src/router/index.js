2da947f849369d973d8318cc844bb290bd3ffe8a
DrKins <52624337+DrKins@users.noreply.github.com>
2019-12-25 19:54:33 +0100

    update router folder

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,48 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Pocetna from '../views/Pocetna.vue'
-import Generiranje from "../views/Generiranje";
-import Izvlacenje from "../views/Izvlacenje.vue";
-import Rezultati from "../views/Rezultati.vue";
-import Posljednja from "../views/Rezultati.vue";
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Pocetna',
-    component: Pocetna
+    name: 'home',
+    component: Home
   },
   {
-    path: '/izvlacenje',
-    name: 'Izvlacenje',
+    path: '/generisanje',
+    name: 'generisanje',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/Izvlacenje.vue')
-    component: Izvlacenje
+    component: () => import(/* webpackChunkName: "about" */ '../views/generisanje.vue')
   },
   {
-    path: '/generiranje',
-    name: 'Generiranje',
-    component: Generiranje
+    path: '/izvlacenje',
+    name: 'izvlacenje',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/izvlacenje.vue')
   },
   {
-    path: '/rezultati',
-    name: 'Rezultati',
-    component: Rezultati
+    path: '/rezultat',
+    name: 'rezultat',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/rezultat.vue')
   },
   {
-    path:'/posljednja',
-    name: 'Posljednja',
-    component: Posljednja
-  }
+    path: '/generisanje0',
+    name: 'generisanje0',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/generisanje0.vue')
+  },
+
 ]
 
 const router = new VueRouter({
